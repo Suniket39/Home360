6da65a0bf95cc2f56de5f336708abdd13dc3dcf8
Suniket Pargaonkar <suniket@kemarautomation.in>
2025-11-26 18:38:08 +0530

    Feat : Added Error Handler Middleware

--- Home360/Home360/Program.cs
+++ Home360/Home360/Program.cs
@@ -1,4 +1,5 @@
 using Home360.API.Core;
+using Home360.API.Core.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,7 @@ if (app.Environment.IsDevelopment())
 
 //Custom Middleware
 app.UseMiddleware<RequestLoggingMiddleware>();
+app.UseMiddleware<ErrorHandlerMiddleware>();    
 app.UseMiddleware<ShortCircuitingMiddleware>();
 
 app.UseHttpsRedirection();
