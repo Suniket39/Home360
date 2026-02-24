65f15f2edadb321f5f33eaeef5afe21923bbb843
Suniket Pargaonkar <suniket95.sp@gmail.com>
2026-02-25 01:53:34 +0530

    Feat: Added Context factory

---
+++ Home360/Home360.Infrastructure/Persistence/DBContext/HomeContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Home360.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Home360.Infrastructure.Persistence
+{
+    public class HomeContextFactory : IHomeContextFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public HomeDbContext CreateDbContext()
+        {
+            var options = new DbContextOptionsBuilder<HomeDbContext>();
+            options.UseSqlServer(_configuration.GetConnectionString("DataSQLContext"));
+
+            return new HomeDbContext(options.Options);
+        }
+    }
+}
---
+++ Home360/Home360.Infrastructure/Persistence/DBContext/IHomeContextFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Home360.Infrastructure.Persistence
+{
+    public interface IHomeContextFactory
+    {
+        HomeDbContext CreateDbContext();
+    }
+}
