3d1d0685766e27edc89914e2998d7873568b552f
Suniket Pargaonkar <suniket@kemarautomation.in>
2026-04-20 19:19:55 +0530

    Fix: made single Unit for multiple measurements

--- Home360/Home360.Domain/Entities/GroceryInventory/GroceryItem.cs
+++ Home360/Home360.Domain/Entities/GroceryInventory/GroceryItem.cs
@@ -4,7 +4,7 @@
     {
         public int ItemId { get; set; }
         public string ItemName { get; set; }
-        public string ItemUnit { get; set; }
+        public string ItemUnit { get; set; } //Kg, Nos, Ltr
         public string ItemDescription { get; set; } = string.Empty;
         public string Status { get; set; }
 
