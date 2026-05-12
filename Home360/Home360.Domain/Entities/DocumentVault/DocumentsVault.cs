fa932e7c806e0dd50580b9ad14918df79af548c1
Suniket Pargaonkar <suniket95.sp@gmail.com>
2026-05-13 01:03:35 +0530

    Feat: added entity configuraton

--- Home360/Home360.Domain/Entities/DocumentVault/DocumentsVault.cs
+++ Home360/Home360.Domain/Entities/DocumentVault/DocumentsVault.cs
@@ -1,12 +1,22 @@
-﻿namespace Home360.Domain.Entities.DocumentVault
+﻿namespace Home360.Domain.Entities
 {
     public class DocumentsVault : CommonEntity
     {
         public int DocumentId { get; set; }
-        public string DocumentName { get; set; }
-        public string DocumentType { get; set; }
+        public string OriginalFileName { get; set; }
+        public string StoredFileName { get; set; }
+        public string RelativePath { get; set; }
+        public string CategoryType { get; set; }
+        public string ContentType { get; set; }
+        public string Extension { get; set; }
         public string DocumentDescription { get; set; } = string.Empty;
-        public string DocumentVersion { get; set; }
-        public string DocumentPath { get; set; }
+        public string DocumentVersion { get; set; } = string.Empty;
+        public long FileSize { get; set; } // Size in bytes
+        public DateTime? LastAccessedDate { get; set; } // Nullable, updated on access
+        public bool IsArchived { get; set; } = false; // Default to not archived
+        public string? Checksum { get; set; } // Optional, for file integrity
+        public bool IsPublic { get; set; } = false; // Default to private
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
     }
 }
---
+++ Home360/Home360.Infrastructure/Persistence/EntityConfiguration/DocumentVault/DocumentVaultConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Home360.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Home360.Infrastructure.Persistence.EntityConfiguration
+{
+    public class DocumentVaultConfiguration : IEntityTypeConfiguration<DocumentsVault>
+    {
+        public void Configure(EntityTypeBuilder<DocumentsVault> builder)
+        {
+            builder.HasKey(x => x.DocumentId);
+            builder.Property(x => x.DocumentId).ValueGeneratedOnAdd();
+            builder.Property(x => x.OriginalFileName).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.StoredFileName).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.RelativePath).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.CategoryType).IsRequired().HasMaxLength(30);
+            builder.Property(x => x.ContentType).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Extension).IsRequired().HasMaxLength(30);
+            builder.Property(x => x.DocumentDescription).IsRequired(false).HasMaxLength(1000);
+            builder.Property(x => x.DocumentVersion).IsRequired(false).HasMaxLength(50);
+            builder.Property(x => x.FileSize).IsRequired();
+            builder.Property(x => x.LastAccessedDate).IsRequired(false);
+            builder.Property(x => x.IsArchived).IsRequired(false);
+            builder.Property(x => x.Checksum).IsRequired(false).HasMaxLength(64);
+            builder.Property(x => x.IsPublic).IsRequired(false);
+
+            builder.HasOne(x => x.User)
+                   .WithMany()
+                   .HasForeignKey(x => x.UserId)
+                   .IsRequired();
+        }
+    }
+}
