7c47a13258c2c2900b873933e9bb7b91e9a2f019
Suniket Pargaonkar <suniket@kemarautomation.in>
2026-06-01 18:30:17 +0530

    Fix: added background image, fix login form

--- Home360UI/src/app/shared/components/form-card/form-card.ts
+++ Home360UI/src/app/shared/components/form-card/form-card.ts
@@ -1,68 +1,49 @@
-import { Component, DebugElement, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
-import { FormControl, FormGroup } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../../core/services/api.service';
 import { ToastifyManager } from '@andreasnicolaou/toastify';
-import { form, Field, FormRoot,FormField , debounce, submit } from '@angular/forms/signals';
+import { form, FormField } from '@angular/forms/signals';
 
 export interface LoginRequestModel {
   username: string;
   password: string;
 }
+
 @Component({
   selector: 'app-form-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
+  imports: [MatCardModule, MatButtonModule, MatInputModule, FormField],
   templateUrl: './form-card.html',
   styleUrl: './form-card.scss',
 })
 export class FormCard implements OnInit{
-
-  form: FormGroup;
   toast : ToastifyManager;
-
   private readonly model = signal<LoginRequestModel>({
     username: '',
     password: ''
   });
 
-  protected readonly loginForm = form<LoginRequestModel>(this.model, schema => {
-    debounce(schema, 300);
-  })
+  readonly loginForm = form(this.model);
 
   constructor(private readonly apiService :ApiService) {
-    this.form = new FormGroup({
-      username: new FormControl(''),
-      password: new FormControl(''),
-    });
-
     this.toast = new ToastifyManager('top-right', {
-      
       closeButton: true,
       withProgressBar: true,
       newestOnTop: true,
-      
     });
-    
   }
 
   ngOnInit(): void {
     
   }
 
-  
-  submit(){
-    this.loadProducts();
-  }
-
-  loadProducts(): void{
-
+  onLogin(): void{
+    debugger
     var body = {
-      username : "Suniket",
-      password: "Pass@123"
+      username : this.model().username,
+      password: this.model().password
     }
 
     this.apiService.post<any>("auth/authenticate", body).subscribe(
@@ -73,15 +54,16 @@ export class FormCard implements OnInit{
           console.log(data);
         },
         error: (err) =>
-          {
-            debugger
-            this.toast.error('Error!', 'Something went wrong, please try again.',
-              {
-                duration: 10,
-                progressBarDuration: 10,
-              }
-            );
-          }
-        });
+        {
+          debugger
+          this.toast.error('Error!', 'Something went wrong, please try again.',
+            {
+              duration: 10,
+              progressBarDuration: 10,
+            }
+          );
+        }
+      }
+    );
   }
 }
